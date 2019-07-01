9184b1f4e8d584ced196ea80b72e6edd8bac7989
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-07-01 17:07:51 +0200

    add spring-initializer folders

---
+++ bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/BakerStreetFighterApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeschool.bakerStreetFighter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BakerStreetFighterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BakerStreetFighterApplication.class, args);
+	}
+
+}
