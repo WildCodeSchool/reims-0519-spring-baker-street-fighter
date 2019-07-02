8e0c2945b8f70509ef045409121342f6ba3732b1
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-07-02 10:52:36 +0200

    add instances

--- bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/repositories/FighterRepository.java
+++ bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/repositories/FighterRepository.java
@@ -11,8 +11,8 @@ class FighterRepository {
 
     public FighterRepository() {
         this.fighters = new ArrayList<Fighter>();
-        fighters.add(new Fighter(1, "Sherlock Holmes"));
-        fighters.add(new Fighter(2, "Professeur Moriarty"));
+        fighters.add(new Fighter(1, "Sherlock Holmes", 0, 0));
+        fighters.add(new Fighter(2, "Professeur Moriarty", 0, 0));
     }
 
 }
