b10da37319ae307be489e9a329ad00ecf2c5604a
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-07-02 10:39:15 +0200

    root for each pages

--- bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/controllers/FighterController.java
+++ bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/controllers/FighterController.java
@@ -7,22 +7,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 class FighterController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String index(Model model) {
         return "index";
     }
 
-    @GetMapping("/home/rules")
+    @GetMapping("/rules")
     public String rules(Model model) {
         return "rules";
     }
 
-    @GetMapping("/home/fight")
+    @GetMapping("/fight")
     public String fight(Model model) {
         return "fight";
     }
 
-    @GetMapping("/home/ranking")
+    @GetMapping("/ranking")
     public String ranking(Model model) {
         return "ranking";
     }
