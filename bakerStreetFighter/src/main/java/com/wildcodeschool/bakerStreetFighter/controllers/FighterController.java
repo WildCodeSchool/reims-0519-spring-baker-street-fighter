6690eb7fe54372baf0dc77bd1425a193eb0b06f1
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-07-02 10:17:30 +0200

    add roots for each pages

--- bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/controllers/FighterController.java
+++ bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/controllers/FighterController.java
@@ -1,8 +1,30 @@
 package com.wildcodeschool.bakerStreetFighter.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 class FighterController {
+
+    @GetMapping("/home")
+    public String index(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/home/rules")
+    public String rules(Model model) {
+        return "rules";
+    }
+
+    @GetMapping("/home/fight")
+    public String fight(Model model) {
+        return "fight";
+    }
+
+    @GetMapping("/home/ranking")
+    public String ranking(Model model) {
+        return "ranking";
+    }
     
 }
