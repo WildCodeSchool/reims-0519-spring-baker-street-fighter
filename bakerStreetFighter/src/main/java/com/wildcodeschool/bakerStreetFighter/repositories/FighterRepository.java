d951065ab72faa6829eb01165d39ffe92861fcc4
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-07-02 09:24:19 +0200

    ADD riskAttack method + safeAttack method

--- bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/repositories/FighterRepository.java
+++ bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/repositories/FighterRepository.java
@@ -1,5 +1,24 @@
 package com.wildcodeschool.bakerStreetFighter.repositories;
 
 class FighterRepository {
-    
+
+    public int getRiskRandomAttack() {
+        double probability = Math.random();
+        int randomRiskAttackDamage = 30;
+        if (probability > 0.5) {
+            return randomRiskAttackDamage;
+        } 
+        return 0;
+    }
+
+    public int getSafeRandomAttack() {
+        double probability = Math.random();
+        int randomSafeAttackDamage = 10;
+        
+        if (probability > 0.2) {
+            return randomSafeAttackDamage;
+        } 
+        return 0;
+    }
+
 }
