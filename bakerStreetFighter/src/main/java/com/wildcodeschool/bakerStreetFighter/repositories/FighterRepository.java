ae5baa573145d8a6c862a9775880109c258ccef7
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-07-03 17:59:25 +0200

    add method selectbyid and selectall

--- bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/repositories/FighterRepository.java
+++ bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/repositories/FighterRepository.java
@@ -1,9 +1,9 @@
 package com.wildcodeschool.bakerStreetFighter.repositories;
 
-import com.wildcodeschool.bakerStreetFighter.entities.Fighter;
-
 import java.util.ArrayList;
 import java.util.List;
+
+import com.wildcodeschool.bakerStreetFighter.entities.Fighter;
 
 class FighterRepository {
     
@@ -13,6 +13,19 @@ class FighterRepository {
         this.fighters = new ArrayList<Fighter>();
         fighters.add(new Fighter(1, "Sherlock Holmes", 0, 0));
         fighters.add(new Fighter(2, "Professeur Moriarty", 0, 0));
+    }
+
+    public List<Fighter> getFighters() {
+        return fighters;
+    }
+
+    public Fighter getFighterById(int id) {
+        for(Fighter fighter : fighters) {
+            if(fighter.getId() == id) {
+                return fighter;
+            }
+        }
+        return null;
     }
 
 }
