c249623b1ed95f4ff6d57624332df2f01a4ef2e5
Youssef Sabouni <youssefsabouni@icloud.com>
2019-07-03 13:12:22 +0200

    created ranking on JSON

--- bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/controllers/RankingController.java
+++ bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/controllers/RankingController.java
@@ -6,6 +6,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.wildcodeschool.bakerStreetFighter.entities.Player;
+
 import java.sql.SQLException;
 
 import org.springframework.http.HttpStatus;
@@ -19,7 +22,7 @@ import org.springframework.web.server.ResponseStatusException;
 @ResponseBody
 public class RankingController<Ranking> {
 
-    private final static String DB_URL = "jdbc:mysql://localhost:3306/wild_db_quest?serverTimezone=GMT";
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/baker_street_fighter?serverTimezone=GMT";
     private final static String DB_USER = "baker";
     private final static String DB_PASSWORD = "Fighter51!";
 
@@ -30,7 +33,7 @@ public class RankingController<Ranking> {
                 DB_URL, DB_USER, DB_PASSWORD
             );
             PreparedStatement statement = connection.prepareStatement(
-                "SELECT * FROM fighter"
+                "SELECT * FROM fighter WHERE name LIKE ?"
             );
         ) {
             statement.setString(1, country);
@@ -43,10 +46,10 @@ public class RankingController<Ranking> {
                 while(resulSet.next()){
                     int id = resulSet.getInt("id");
                     String name = resulSet.getString("name");
-                    int totalGamesPlayed = resulSet.getInt("totalGamesPlayed");
-                    int victory = resulSet.getInt("victory");
-                    int defeat = resulSet.getInt("defeat");
-                    rankings.add(new Player.Player(id, name, totalGamesPlayed, victory, defeat));
+                    String nickname = resulSet.getString("nickname");
+                    int victory_count = resulSet.getInt("victory_count");
+                    int defeat_count = resulSet.getInt("defeat_count");
+                    rankings.add(new Ranking(id, nickname, name, victory_count, defeat_count));
                 }
     
                 return rankings;
@@ -58,4 +61,63 @@ public class RankingController<Ranking> {
             );
         }
     }
+
+    class Ranking {
+
+        private int id;
+        private String nickname;
+        private String name;
+        private int victory_count;
+        private int defeat_count;
+    
+        public Ranking(int id, String nickname, String name, int victory_count, int defeat_count) {
+            this.id = id;
+            this.nickname = nickname;
+            this.name = name;
+            this.victory_count = victory_count;
+            this.defeat_count = defeat_count;
+        }
+
+		public int getId() {
+			return id;
+		}
+
+		public void setId(int id) {
+			this.id = id;
+		}
+
+		public String getNickname() {
+			return nickname;
+		}
+
+		public void setNickname(String nickname) {
+			this.nickname = nickname;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public int getVictory_count() {
+			return victory_count;
+		}
+
+		public void setVictory_count(int victory_count) {
+			this.victory_count = victory_count;
+		}
+
+		public int getDefeat_count() {
+			return defeat_count;
+		}
+
+		public void setDefeat_count(int defeat_count) {
+			this.defeat_count = defeat_count;
+		}
+
+    }
+
 }
