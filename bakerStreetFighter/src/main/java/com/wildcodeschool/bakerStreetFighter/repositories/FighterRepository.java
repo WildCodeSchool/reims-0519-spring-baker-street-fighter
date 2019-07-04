d05486644da881d88e6802cffdd3351d3795e7e9
Clément Bouakil <bouakil.clement.pro@gmail.com>
2019-07-04 18:13:31 +0200

    delete useless file repo + add constance BDD in fighterRepo

--- bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/repositories/FighterRepository.java
+++ bakerStreetFighter/src/main/java/com/wildcodeschool/bakerStreetFighter/repositories/FighterRepository.java
@@ -6,6 +6,10 @@ import java.util.List;
 import com.wildcodeschool.bakerStreetFighter.entities.Fighter;
 
 public class FighterRepository {
+
+    private final static String DB_URL = "jdbc:mysql://localhost:3306/wild_db_quest?serverTimezone=GMT";
+    private final static String DB_USER = "baker";
+    private final static String DB_PASSWORD = "Fighter51!";
     
     private List<Fighter> fighters;
 
